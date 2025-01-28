ff0b342591466bcd8836ace4eb5103e644748fb8
Hephzy-1 <hzdelight01@gmail.com>
2025-01-28 11:48:09 +0100

    feat: user auth

--- src/routes/customer.js
+++ src/routes/customer.js
@@ -3,14 +3,9 @@ import { login, register, resendOTP, verifyOTP, forgetPassword, resetPassword } 
 import env from '../config/env.js';
 import upload from '../utils/multer.js';
 import { ErrorResponse } from '../utils/errorResponse.js';
-// import cache from '../middlewares/cache';
 
 export const route = express.Router();
 
-if (!env.CACHEKEY) {
-  throw new ErrorResponse("Cache unique key is required", 500);
-  
-}
 route.post('/register', register);
 route.post('/login', login);
 route.route('/otp/:id')
